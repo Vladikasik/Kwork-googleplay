2e90c2ea042c91fa5f5a9ef2f0c4475970670571
Vlad <vsakayafignavlad@gmail.com>
2021-08-25 19:12:58 +0300

    done with instructions

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import play_scraper
 import json
-with open('input_file', 'r', encoding='utf-8') as file:
-    for query in file.readlines():
-        answer = play_scraper.search(query=query, page=12, detailed=True)
-        for app in answer:
-            file = open(f'exit/{app["app_id"]}.json', 'w')
-            out = {}
-            for i, j in app.items():
-                try:
-                    j = j.decode('utf-8')
-                except:
-                    pass
-                finally:
-                    out[i] = j
-            json.dump(out, file, indent=4, sort_keys=True)
+
+
+def main():
+    with open('input_file', 'r', encoding='utf-8') as file:
+        for query in file.readlines():
+            answer = play_scraper.search(query=query, page=12, detailed=True)
+            if answer:
+                for app in answer:
+                    file = open(f'exit/{app["app_id"]}.json', 'w')
+                    out = {}
+                    for i, j in app.items():
+                        try:
+                            j = j.decode('utf-8')
+                        except:
+                            pass
+                        finally:
+                            out[i] = j
+                    json.dump(out, file, indent=4, sort_keys=True)
+            else:
+                main()
+
+if __name__ == '__main__':
+    main()
